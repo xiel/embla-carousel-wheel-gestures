6e3a2e4e9e94af8185aabc436ddb5f06b267d06b
Felix Leupold <felix@xiel.de>
2020-06-10 20:43:39 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['eslint:recommended', 'react-app', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'no-shadow': 'error',
+    'react/self-closing-comp': ['error', { component: true, html: true }],
+    'react/jsx-boolean-value': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    'simple-import-sort/sort': 'error',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-duplicates': 'warn',
+    'import/no-webpack-loader-syntax': 'warn',
+  },
+  settings: {
+    react: {
+      version: '999.999.999',
+    },
+  },
+}
