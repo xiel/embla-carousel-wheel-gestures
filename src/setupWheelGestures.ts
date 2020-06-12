42b3385b3958c1d897e84897e4252e33f7de4213
Felix Leupold <felix@xiel.de>
2020-06-12 20:22:53 +0200

    install embla 3.0.0

--- src/setupWheelGestures.ts
+++ src/setupWheelGestures.ts
@@ -11,11 +11,9 @@ export function setupWheelGestures(embla: EmblaCarousel, {} = {}) {
   let cleanupFn = () => {}
 
   function initWheelGestures() {
-    // @ts-expect-error
     embla.on('reInit', reInit)
     embla.on('destroy', cleanup)
 
-    // @ts-expect-error
     const engine = embla.dangerouslyGetEngine()
     const targetNode = embla.containerNode().parentNode as Element
 
@@ -50,7 +48,6 @@ export function setupWheelGestures(embla: EmblaCarousel, {} = {}) {
     function cleanup() {
       unobserveTargetNode()
       offWheel()
-      // @ts-expect-error
       embla.off('reInit', reInit)
       embla.off('destroy', cleanup)
     }
