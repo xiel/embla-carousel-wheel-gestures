13aef98435d2e74de988a5bd14de4d97cbde69bc
Felix Leupold <felix@xiel.de>
2022-03-18 22:02:21 +0100

    #159 Plugin in Shadow Dom (#162)
    * fix: support embla in shadow dom | add composed flag
    * fix: add shadow demo
    * released v2.1.1

---
+++ docs/vanilla/src/shadow/index.ts
@@ -0,0 +1,43 @@
+import EmblaCarousel from 'embla-carousel'
+import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
+
+function initializeEmbla(root: ShadowRoot | HTMLElement) {
+  const template = document.getElementById('embla-template') as HTMLTemplateElement
+
+  root.appendChild(template.content.cloneNode(true))
+  const wrapper = root.querySelector('.embla')! as HTMLElement
+  const viewport = wrapper.querySelector('.embla__viewport')!
+
+  EmblaCarousel(
+    viewport as HTMLElement,
+    {
+      loop: true,
+      skipSnaps: true,
+      dragFree: true,
+    },
+    [WheelGesturesPlugin()]
+  )
+}
+
+customElements.define(
+  'embla-carousel-with-shadow',
+  class extends HTMLElement {
+    public constructor() {
+      super()
+      this.attachShadow({ mode: 'open' })
+    }
+
+    private connectedCallback() {
+      initializeEmbla(this.shadowRoot)
+    }
+  }
+)
+
+customElements.define(
+  'embla-carousel-no-shadow',
+  class extends HTMLElement {
+    private connectedCallback() {
+      initializeEmbla(this)
+    }
+  }
+)
