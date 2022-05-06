b260d472a8771caacd1438f321369e8c5893ee1a
Felix Leupold <felix@xiel.de>
2022-05-06 15:50:57 +0200

    test: target option

--- embla-carousel-wheel-gestures/test/options.test.ts
+++ embla-carousel-wheel-gestures/test/options.test.ts
@@ -1,10 +1,11 @@
 import { WheelGesturesPlugin } from '../src'
 
 test('Passed options merged with defaults', () => {
-  const wheelGestures = WheelGesturesPlugin({ forceWheelAxis: 'y' })
+  const wheelGestures = WheelGesturesPlugin({ forceWheelAxis: 'y', target: document.body })
   expect(wheelGestures.options).toMatchInlineSnapshot(`
     Object {
       "forceWheelAxis": "y",
+      "target": <body />,
       "wheelDraggingClass": "is-wheel-dragging",
     }
   `)
@@ -15,6 +16,7 @@ test('No passed options, no global options', () => {
   expect(wheelGestures.options).toMatchInlineSnapshot(`
     Object {
       "forceWheelAxis": undefined,
+      "target": undefined,
       "wheelDraggingClass": "is-wheel-dragging",
     }
   `)
@@ -27,19 +29,23 @@ test('Global options are merged correctly', () => {
   }
 
   const wheelGestures = WheelGesturesPlugin()
+
   const wheelGestures2 = WheelGesturesPlugin({
     wheelDraggingClass: 'passed-class',
+    target: document.body,
   })
 
   expect(wheelGestures.options).toMatchInlineSnapshot(`
     Object {
       "forceWheelAxis": "x",
+      "target": undefined,
       "wheelDraggingClass": "abc",
     }
   `)
   expect(wheelGestures2.options).toMatchInlineSnapshot(`
     Object {
       "forceWheelAxis": "x",
+      "target": <body />,
       "wheelDraggingClass": "passed-class",
     }
   `)
